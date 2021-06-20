e4de6e9ab40f5e3b22af3f7a3564dfdd7977a369
DESKTOP-K1BDLKT\황세훈 <hwangbird99@gmail.com>
2021-06-20 18:07:15 +0900

    index 만들기

--- mysite/blog/static/js/foodbox.js
+++ mysite/blog/static/js/foodbox.js
@@ -1,1 +1,37 @@
 console.log(10);
+
+const food_img_slide=document.querySelector('.food-img-slide');
+
+//jeyukbokkeum-img
+//dakbogum-img
+// samgub-img
+// boggumbab-img
+const food_img_array=['jeyukbokkeum-img','dakbogum-img','samgub-img','boggumbab-img'];
+
+food_img_slide.classList.add(food_img_array[0]);
+
+function food(){
+  if(food_img_slide.classList.contains(food_img_array[0])){
+    food_img_slide.classList.remove(food_img_array[0]);
+    food_img_slide.classList.add(food_img_array[1]);
+  }
+
+  else if(food_img_slide.classList.contains(food_img_array[1])){
+    food_img_slide.classList.remove(food_img_array[1]);
+    food_img_slide.classList.add(food_img_array[2]);
+  }
+
+  else if(food_img_slide.classList.contains(food_img_array[2])){
+    food_img_slide.classList.remove(food_img_array[2]);
+    food_img_slide.classList.add(food_img_array[3]);
+  }
+
+  else if(food_img_slide.classList.contains(food_img_array[3])){
+    food_img_slide.classList.remove(food_img_array[3]);
+    food_img_slide.classList.add(food_img_array[0]);
+  }
+
+}
+
+let control;
+control=setInterval(food,5000);

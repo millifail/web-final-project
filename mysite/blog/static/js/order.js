43d0e04e02ddbbafc09ec5c6cd29ea0e18b4707a
DESKTOP-K1BDLKT\황세훈 <hwangbird99@gmail.com>
2021-06-22 02:54:25 +0900

    total price 계산 js로 구현

--- mysite/blog/static/js/order.js
+++ mysite/blog/static/js/order.js
@@ -3,12 +3,18 @@ const add_jeyuk_price=document.querySelector('.add-food-jeyuk-price');
 const delete_jeyuk_price=document.querySelector('.delete-food-jeyuk-price');
 const add_dakbogum_price=document.querySelector('.add-food-dakbogum-price');
 const delete_dakbogum_price=document.querySelector('.delete-food-dakbogum-price');
-
 const add_samgub_price=document.querySelector('.add-food-samgub-price');
 const delete_samgub_price=document.querySelector('.delete-food-samgub-price');
-
 const add_bogumbab_price=document.querySelector('.add-food-bogumbab-price');
 const delete_bogumbab_price=document.querySelector('.delete-food-bogumbab-price');
+const side_dishes_kinds=document.querySelector('.food-align.side-dishes-kinds');
+const rice_dishes_kinds=document.querySelector('.food-align.rice-dishes-kinds');
+const soup_dishes_kinds=document.querySelector('.food-align.soup-dishes-kinds');
+const dessert_dishes_kinds=document.querySelector('.food-align.dessert-dishes-kinds');
+let side_num=0; //side menu 개수
+let rice_num=0;
+let soup_num=0;
+let dessert_num=0;
 
 let food_num=[0,0,0,0];    //제육, 닭볶음탕, 우삼겹, 볶음밥   개수
 
@@ -16,7 +22,7 @@ let price=0;
 total_price.innerHTML='total: '+price;
 
 function calculate_price(){
-  price=(food_num[0]*7000) + (food_num[1]*8000)+ (food_num[2]*6000)+(food_num[3]*7000);
+  price=(food_num[0]*7000) + (food_num[1]*8000)+ (food_num[2]*6000)+(food_num[3]*7000)+ (side_num*500)+(rice_num*1000) +(soup_num*1000)+(dessert_num*0);
   total_price.innerHTML='total: '+price;
 }
 
@@ -70,4 +76,137 @@ delete_bogumbab_price.addEventListener('click',function(){
 });
 
 
-console.log(food_num);
+console.log(side_dishes_kinds.childNodes[1]);
+console.log(side_dishes_kinds.childNodes[3]);
+console.log(side_dishes_kinds.childNodes[5]);
+console.log(side_dishes_kinds.childNodes[7]);
+
+//side menu  기능 추가
+for(let i =0; i<3; i++){
+  let j=2*i+1;
+  side_dishes_kinds.childNodes[j].addEventListener('click',function(e){
+    if(side_dishes_kinds.childNodes[j].classList.contains('add')){//제거
+        side_dishes_kinds.childNodes[j].classList.remove('add');
+        side_num-=1;
+        calculate_price();
+    }
+    else{ //선택;
+      side_dishes_kinds.childNodes[7].classList.remove('add');
+      side_dishes_kinds.childNodes[j].classList.add('add');
+      side_num+=1;
+      calculate_price();
+    }
+
+  });
+
+}
+side_dishes_kinds.childNodes[7].addEventListener('click',function(e){
+    side_dishes_kinds.childNodes[7].classList.add('add');
+    side_dishes_kinds.childNodes[1].classList.remove('add')
+    side_dishes_kinds.childNodes[3].classList.remove('add')
+    side_dishes_kinds.childNodes[5].classList.remove('add')
+    side_num=0;
+    calculate_price();
+
+});
+
+
+//rice menu 기능추가
+
+for(let i =0; i<3; i++){
+  let j=2*i+1;
+  rice_dishes_kinds.childNodes[j].addEventListener('click',function(e){
+    if(rice_dishes_kinds.childNodes[j].classList.contains('add')){//제거
+        rice_dishes_kinds.childNodes[j].classList.remove('add');
+        rice_num-=1;
+        calculate_price();
+    }
+    else{ //선택;
+      rice_dishes_kinds.childNodes[7].classList.remove('add');
+      rice_dishes_kinds.childNodes[j].classList.add('add');
+      rice_num+=1;
+      calculate_price();
+    }
+
+  });
+
+}
+rice_dishes_kinds.childNodes[7].addEventListener('click',function(e){
+    rice_dishes_kinds.childNodes[7].classList.add('add');
+    rice_dishes_kinds.childNodes[1].classList.remove('add')
+    rice_dishes_kinds.childNodes[3].classList.remove('add')
+    rice_dishes_kinds.childNodes[5].classList.remove('add')
+    rice_num=0;
+    calculate_price();
+
+});
+
+//soup menu 기능 추가
+for(let i =0; i<3; i++){
+  let j=2*i+1;
+  soup_dishes_kinds.childNodes[j].addEventListener('click',function(e){
+    if(soup_dishes_kinds.childNodes[j].classList.contains('add')){//제거
+        soup_dishes_kinds.childNodes[j].classList.remove('add');
+        soup_num-=1;
+        calculate_price();
+    }
+    else{ //선택;
+      soup_dishes_kinds.childNodes[7].classList.remove('add');
+      soup_dishes_kinds.childNodes[j].classList.add('add');
+      soup_num+=1;
+      calculate_price();
+    }
+
+  });
+
+}
+soup_dishes_kinds.childNodes[7].addEventListener('click',function(e){
+    soup_dishes_kinds.childNodes[7].classList.add('add');
+    soup_dishes_kinds.childNodes[1].classList.remove('add')
+    soup_dishes_kinds.childNodes[3].classList.remove('add')
+    soup_dishes_kinds.childNodes[5].classList.remove('add')
+    soup_num=0;
+    calculate_price();
+
+});
+
+//dessert menu 기능 추가
+dessert_dishes_kinds.childNodes[1].addEventListener('click',function(e){
+    dessert_dishes_kinds.childNodes[1].classList.add('add');
+    dessert_dishes_kinds.childNodes[3].classList.remove('add');
+    dessert_dishes_kinds.childNodes[5].classList.remove('add');
+    dessert_dishes_kinds.childNodes[7].classList.remove('add');
+    dessert_num=1;
+    calculate_price();
+
+});
+
+dessert_dishes_kinds.childNodes[3].addEventListener('click',function(e){
+    dessert_dishes_kinds.childNodes[3].classList.add('add');
+    dessert_dishes_kinds.childNodes[1].classList.remove('add');
+    dessert_dishes_kinds.childNodes[5].classList.remove('add');
+    dessert_dishes_kinds.childNodes[7].classList.remove('add');
+    dessert_num=1;
+    calculate_price();
+
+});
+
+dessert_dishes_kinds.childNodes[5].addEventListener('click',function(e){
+    dessert_dishes_kinds.childNodes[5].classList.add('add');
+    dessert_dishes_kinds.childNodes[1].classList.remove('add');
+    dessert_dishes_kinds.childNodes[3].classList.remove('add');
+    dessert_dishes_kinds.childNodes[7].classList.remove('add');
+    dessert_num=1;
+    calculate_price();
+
+});
+
+dessert_dishes_kinds.childNodes[7].addEventListener('click',function(e){
+    dessert_dishes_kinds.childNodes[7].classList.add('add');
+    dessert_dishes_kinds.childNodes[1].classList.remove('add');
+    dessert_dishes_kinds.childNodes[3].classList.remove('add');
+    dessert_dishes_kinds.childNodes[5].classList.remove('add');
+    dessert_num=0;
+    calculate_price();
+
+});
